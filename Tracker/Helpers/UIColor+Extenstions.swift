ab54ceed6612f4c39c2c05fff4f5f459157c45c5
Andrey Sysoev <andrey@sysoev.ru>
2023-03-24 13:33:12 +0300

    added LaunchScreen layout

---
+++ Tracker/Helpers/UIColor+Extenstions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extenstions.swift
+//  Tracker
+//
+//  Created by Andrey Sysoev on 24.03.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let black = UIColor(named: "Black")!
+    static let gray = UIColor(named: "Gray")!
+    static let lightGray = UIColor(named: "Light Gray")!
+    static let white = UIColor(named: "White")!
+    static let background = UIColor(named: "Background")!
+    static let red = UIColor(named: "Red")!
+    static let blue = UIColor(named: "Blue")!
+}
