4b6156554916933a38b26f2967639ce1fcd5de89
Andrey Sysoev <andrey@sysoev.ru>
2023-03-25 14:00:33 +0300

    sprint_14: added UICollectionView

--- Tracker/Trackers/TrackersViewController.swift
+++ Tracker/Trackers/TrackersViewController.swift
@@ -53,6 +53,13 @@ final class TrackersViewController: UIViewController {
         return view
     }()
     
+    private let collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -79,20 +86,30 @@ private extension TrackersViewController {
         view.addSubview(titleLabel)
         view.addSubview(datePicker)
         view.addSubview(searchBar)
+        view.addSubview(collectionView)
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
+            // plusButton
             plusButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 13),
             plusButton.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            // titleLabel
             titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             titleLabel.topAnchor.constraint(equalTo: plusButton.bottomAnchor, constant: 13),
+            // datePicker
             datePicker.widthAnchor.constraint(equalToConstant: 120),
             datePicker.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
             datePicker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            // searchBar
             searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
             searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
             searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            // collectionView
+            collectionView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: datePicker.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
@@ -103,5 +120,11 @@ extension TrackersViewController: UISearchBarDelegate {
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         searchBar.setShowsCancelButton(true, animated: true)
         return true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
     }
 }
