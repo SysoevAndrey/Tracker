8afac7c1c502f9ffd616fdd7de21c1004ac592af
Andrey Sysoev <andrey@sysoev.ru>
2023-04-01 08:34:04 +0300

    sprint_14: fixed picker date

--- Tracker/Models/TrackerCategory.swift
+++ Tracker/Models/TrackerCategory.swift
@@ -17,35 +17,35 @@ extension TrackerCategory {
         TrackerCategory(
             label: "Домашний уют",
             trackers: [
-                Tracker(
-                    label: "Поливать растения",
-                    emoji: "❤️",
-                    color: UIColor(named: "Color selection 5")!,
-                    schedule: [.saturday]
-                )
+//                Tracker(
+//                    label: "Поливать растения",
+//                    emoji: "❤️",
+//                    color: UIColor(named: "Color selection 5")!,
+//                    schedule: [.saturday]
+//                )
             ]
         ),
         TrackerCategory(
             label: "Радостные мелочи",
             trackers: [
-                Tracker(
-                    label: "Кошка заслонила камеру на созвоне",
-                    emoji: "😻",
-                    color: UIColor(named: "Color selection 2")!,
-                    schedule: nil
-                ),
-                Tracker(
-                    label: "Бабушка прислала открытку в вотсапе",
-                    emoji: "🌺",
-                    color: UIColor(named: "Color selection 1")!,
-                    schedule: nil
-                ),
-                Tracker(
-                    label: "Свидания в апреле",
-                    emoji: "❤️",
-                    color: UIColor(named: "Color selection 14")!,
-                    schedule: nil
-                ),
+//                Tracker(
+//                    label: "Кошка заслонила камеру на созвоне",
+//                    emoji: "😻",
+//                    color: UIColor(named: "Color selection 2")!,
+//                    schedule: nil
+//                ),
+//                Tracker(
+//                    label: "Бабушка прислала открытку в вотсапе",
+//                    emoji: "🌺",
+//                    color: UIColor(named: "Color selection 1")!,
+//                    schedule: nil
+//                ),
+//                Tracker(
+//                    label: "Свидания в апреле",
+//                    emoji: "❤️",
+//                    color: UIColor(named: "Color selection 14")!,
+//                    schedule: nil
+//                ),
             ]
         )
     ]
